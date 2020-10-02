2150b471182b47877c0f5bc48d8dbd962a1a91c8
urmi-21 <mrbains21@gmail.com>
2020-10-01 23:40:32 -0500

    update

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -16,6 +16,11 @@ python compare_fasta_files.py $2/getorf_d $2/orfm_d $2/orfipy_d
 echo "Comparing peptide sequences"
 python compare_fasta_files.py $2/getorf_p $2/orfm_p $2/orfipy_p
 
+echo "Comparing nucleotide sequences"
+python compare_fasta_files.py $2/getorf_3_d $2/orfipy_3_d
+echo "Comparing peptide sequences"
+python compare_fasta_files.py $2/getorf_3_p $2/orfipy_3_p
+
 #get pyrpipelog file
 plog=$(ls -Art pyrpipe_logs/*pyrpipe.log | tail -n 1)
 
