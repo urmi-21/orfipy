1a62941bd989d9478cc60d53fba5464d10370d63
urmi-21 <mrbains21@gmail.com>
2020-10-06 17:08:20 -0500

    test

--- tests/test_orfs.py
+++ tests/test_orfs.py
@@ -3,23 +3,34 @@
 """
 @author: usingh
 """
-import orfipy
-import orfipy.findorfs as fo
 import orfipy_core as oc
 
 seq='ATGTTTATGAAATAGAACTAAATGCCCATG'
-orfs_pos1=[(0, 12, 1, 'ATG', 'TAG', 'complete', 12), 
-           (1, 7, 2, 'TGT', 'TGA', '5-prime-partial', 6), 
-           (2, 29, 3, 'GTT', 'NA', '5-prime-partial', 27), 
-           (10, 28, 2, 'AAT', 'NA', '5-prime-partial', 18), 
-           (15, 18, 1, 'AAC', 'TAA', '5-prime-partial', 3), 
-           (21, 30, 1, 'ATG', 'NA', '3-prime-partial', 9)] 
-orfs_seq1=['ATGTTTATGAAA', 'TGTTTA', 'GTTTATGAAATAGAACTAAATGCCCAT', 'AATAGAACTAAATGCCCA', 'AAC', 'ATGCCCATG']
+seq_rc='CATGGGCATTTAGTTCTATTTCATAAACAT'
 
 
+#start_search(seq,seq_rc,seqname,minlen,maxlen,strand,starts,stops,table,include_stop,partial3,partial5,find_between_stops,out_opts)
 def test_orf_search():
-    result=oc.get_orfsc(seq,"seq1",0,10000,rev_com=False, partial3=True, partial5=True,return_seqs=True)
+    result=oc.start_search(seq,
+                    seq_rc,
+                    'test',
+                    0,
+                    100000,
+                    'b',
+                    ['ATG'],
+                    ['TAG'],
+                    '1',
+                    True,
+                    True,
+                    False,
+                    False,
+                    [False,False,False,False,False]       
+        
+        )
+    
     print(result)
-    assert result[0]==orfs_pos1
-    assert result[1]==orfs_seq1
-
+    print(result[0])
+    print(len(result[0].split('\n')))
+    assert len(result[0].split('\n'))==3
+    
+    
