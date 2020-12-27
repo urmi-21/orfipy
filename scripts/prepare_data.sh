8b3d271556765c076d9ba85985154bdd73e1d289
urmi-21 <mrbains21@gmail.com>
2020-12-27 15:28:06 -0600

    update scripts

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -2,10 +2,10 @@
 
 mkdir -p ../testdata/bmdata
 
-wget ftp://ftp.ensembl.org/pub/release-101/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz -O ../testdata/bmdata/human.gz
-gunzip --force ../testdata/bmdata/human.gz
-wget ftp://ftp.ensemblgenomes.org/pub/plants/release-48/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa.gz -O ../testdata/bmdata/at.gz
-gunzip --force ../testdata/bmdata/at.gz
+wget ftp://ftp.ensembl.org/pub/release-101/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz -O ../testdata/bmdata/human.fa.gz
+gunzip --force ../testdata/bmdata/human.fa.gz
+wget ftp://ftp.ensemblgenomes.org/pub/plants/release-48/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa.gz -O ../testdata/bmdata/at.fa.gz
+gunzip --force ../testdata/bmdata/at.fa.gz
 
 cp download_Bac.sh ../testdata/bmdata/
 cd ../testdata/bmdata/
