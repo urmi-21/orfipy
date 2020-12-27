9f7c8f34f3d097965600d8435f2c690ceace5231
urmi-21 <mrbains21@gmail.com>
2020-12-27 16:47:50 -0600

    update

---
+++ scripts/basic_benchmark.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#example: bash basic_benchmark.sh ../testdata/bmdata/human.fa 99 human_basic_out
+
+infile=$1
+min=$2
+outdir=$3
+
+mkdir -p $3
+time orfipy --min $2 --bed b --outdir $3 --between-stops $1
+time orfipy --min $2 --dna d --pep p --outdir $3 --between-stops $1
+time orfm -m $2 -t "$3/orfm_d" $1 > "$3/orfm_p"
+time ( getorf -find 0 -min $2 -outseq "$3/getorf_p" -sequence $1 ; getorf -find 2 -min $2 -outseq "$3/getorf_d" -sequence $1 )
+
