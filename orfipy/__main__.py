9ff61ed1279a12bcdadb0df4ad71d24329ba93ca
urmi-21 <mrbains21@gmail.com>
2021-07-09 11:36:03 -0500

    add ignore case option to menu for soft-masked sequences

--- orfipy/__main__.py
+++ orfipy/__main__.py
@@ -183,6 +183,7 @@ def main():
     parser.add_argument("--min", help="Minimum length of ORF, excluding stop codon (nucleotide)\nDefault: 30",default=30)
     parser.add_argument("--max", help="Maximum length of ORF, excluding stop codon (nucleotide)\nDefault: 1,000,000,000",default=1000000000)
     parser.add_argument("--strand", help="Strands to find ORFs [(f)orward,(r)everse,(b)oth]\nDefault: b",default='b',choices=['f', 'r', 'b'])
+    parser.add_argument("--ignore-case", help="Ignore case and find ORFs in lower case sequences too. Useful for soft-masked sequences.\nDefault: False",default=False,dest='ignore_case', action='store_true')
     #parser.add_argument("--nested", help="Output nested and overlapping ORFs in the same frame \nDefault: False",default=False,dest='nested', action='store_true')
     parser.add_argument("--partial-3", help="Output ORFs with a start codon but lacking an inframe stop codon. E.g. \"ATG TTT AAA\"\nDefault: False",default=False,dest='partial3', action='store_true')
     parser.add_argument("--partial-5", help="Output ORFs with an inframe stop codon lacking an inframe start codon. E.g. \"TTT AAA TAG\"\nDefault: False",default=False,dest='partial5', action='store_true')
